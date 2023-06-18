34d0d2c3df1f307d78cf54d6870beabb06581b9f
Piyush Rai <111386563+piyushrai007@users.noreply.github.com>
2023-06-18 20:53:38 +0530

    Update kaden's.py

--- ARRAY/KADEN'SALGO/kaden's.py
+++ ARRAY/KADEN'SALGO/kaden's.py
@@ -1,1 +1,11 @@
 #kadens algorithim
+def maxSubArray(self, nums: List[int]) -> int:
+        max = -10000000000
+        sum = 0
+        for i in range(len(nums)):
+            sum = sum +nums[i]
+            if sum >max:
+                max = sum
+            if sum <0:
+                sum =0
+        return max 
